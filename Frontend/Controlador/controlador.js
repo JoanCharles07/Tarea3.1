2fa7015aa79ccd2aa5abfdc3aebff4523a23fe91
JoanCharles <JoanCharles07@hotmail.com>
2023-11-22 23:43:01 +0100

    Añadido header y nav, imagenes y estilos principal

--- Frontend/Controlador/controlador.js
+++ Frontend/Controlador/controlador.js
@@ -1,3 +1,47 @@
 /**
- * @file Este script controlará las interacciones de los usuarios y responderlas
+ * @file Este script controlará las interacciones entre usuario y aplicación.
+ * @description Este script controla la interaccion de datos tanto entre la vista y el modelo
+ * como entre distintos modelos, haciendo este su punto de unión. Además cuenta con una función donde
+ * se llama a todas las funciones comunes a todas ls vistas.
+ * @author Juan Carlos Rodríguez Miranda.
+ * @version 1.0.0
 */
+
+
+//Importaciones necesarias para el funcionamiento de controlador.js
+
+/**
+ * Esta función nos aseguraremos con la promesa que se ejecute antes que cualquier otra cosa.
+ * @returns Devuelve el resultado de la promesa
+ */
+function requerimientosComunes(){
+    return import("../Vistas/plantillaGeneral.js")
+    .then((metodosPlantilla) => {
+        
+        return metodosPlantilla.imprimirCabezera();
+       
+      })
+    .catch(error => console.log(error));
+}
+/**
+ * Esta función se encargará de activar las distintas funciones según la vista en la que este.
+ * Pero primero realizará una promesa para que se ejecuten todas las acciones comunes.
+ *  
+ * @throws(error)Saltará si alguna de las promesas no puede llevar a cabo su función.
+ */
+function interaccionesControlador(){
+    requerimientosComunes()
+    .then((respuesta)=>{
+        console.log(respuesta);
+    })
+    .then(()=>{
+        
+    if(window.location.pathname.includes("index.html")){
+
+        console.log("Hola");
+    }
+    })
+    
+
+}
+interaccionesControlador();
---
+++ Frontend/Vistas/plantillaGeneral.js
@@ -0,0 +1,78 @@
+/**
+ * @file Este script añadirá al DOM zonas comunes a todos las vistas html.
+* @description Este script añadirá mediante innerHTML zonas como header,nav y footer, que son comunes en todos los html.
+* @author Juan Carlos Rodríguez Miranda.
+* @version 1.0.0
+*/
+
+export function imprimirCabezera() {
+
+    return new Promise((resolve, reject) => {
+        try {
+
+            let headerNav = `<header>
+            <div class="divheader" id="containerLogo"><img id="logo"   src="Recursos/Imagenes/logo.webp" alt="Logo de la tierra"></div>
+            <div class="divheader" id="containerBuscador"><input type="text" name="texto" id="buscador"><img id="lupa"
+            src="Recursos/Imagenes//lupa.webp"  class="iconosHeader" alt=""> </div>
+            <div class="divheader" id="containerSpan"><span id="spanBienvenida" class="bienvenida">Iniciar sesión</span></div>
+            <div class="divheader" id="containerOpciones">
+    
+            <ul id="iconos">
+                <li id="container_lista">
+                    <img id="inicio" class="iconosHeader" src="Recursos/Imagenes/InicioSesion.webp" alt="">
+                    <ul id="lista">
+                    
+                    </ul>
+                </li>
+                <li> 
+                    <img id="accesibilidad" class="iconosHeader" src="Recursos/Imagenes/accesibilidad.webp" alt="">
+                    <ul id="lista2">
+                        <li class="listaIconos">texto texto largo largo</li>
+                        <li class="listaIconos">texto</li>
+                        <li class="listaIconos">texto</li>
+                        <li class="listaIconos">texto</li>
+                    </ul>
+                </li>
+                <li><img id="carrito" src="Recursos/Imagenes/CarritoCompra.webp"  class="iconosHeader" alt="">
+                    <div id="cantidadProductosCarro">0</div></li>
+            </ul>
+            </div></header>`;
+
+            headerNav += `
+            <nav>
+            <div id="boton"><a href="Vistas/index.html">Tienda</a></div>
+            <div id="boton"><a href="Vistas/sobreNosotros.html">Sobre nosotros</a></div>
+            <div id="boton"><a href="Vistas/noticias.html">Noticias</a></div>
+            <div id="boton"><a href="">Contacto</a></div>
+            </nav>
+            `;
+            document.body.innerHTML=headerNav;
+        } catch (error) {
+
+        }
+    });
+}
+
+function imprimirCabezera2() {
+    return new Promise((resolve, reject) => {
+        try {
+            let respuesta = "Yo segundo";
+
+            resolve(respuesta);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+export function imprimirCabezera3() {
+    return new Promise((resolve, reject) => {
+        try {
+
+            let respuesta = "Yo tercero";
+            resolve(respuesta);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
