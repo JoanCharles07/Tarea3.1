2ff79f626af36c6332b813d90c9c336fe52c96c3
JoanCharles <JoanCharles07@hotmail.com>
2023-11-23 15:27:25 +0100

    Funciones de encriptación

--- Backend/Controlador/controlador.php
+++ Backend/Controlador/controlador.php
@@ -6,13 +6,14 @@
  * @version 1.0.0
  */
 require_once '../Conf/conn.php';
-//include_once '../Modelo/funciones.php';
+include_once '../Modelo/funciones.php';
 include_once '../Modelo/funcionesBBDD.php';
 header('Content-Type: application/json');
 session_start();
 $datos=json_decode(file_get_contents('php://input'));
 if($datos->llamada=="productos"){
     $respuesta=recuperarProductos();
+    $respuesta=encriptarTodasPalabras($respuesta);
     echo json_encode($respuesta);
 }
 
--- Backend/Modelo/funciones.php
+++ Backend/Modelo/funciones.php
@@ -8,3 +8,34 @@
  * @author Juan Carlos Rodríguez Miranda
  * @version 1.0.0
  */
+
+/**
+ * Encripta cualquier String.
+ *
+ * Esta función encripta cualquier String que llegue como parametro usando hash con una palabra escogida por nosotros.
+ *
+ * @param String palabra que queremos encriptar
+ * @return String palabra encriptada.
+ */
+
+ function encriptarPalabra($palabra){
+    $WORD = "LaBellaVida8";
+    $retorno=hash('sha512',$WORD.$palabra);
+    return $retorno;
+ }
+
+ /**
+ * Encripta cualquier Array.
+ *
+ * Esta función encripta cualquier Array mediante un bucle for que llama sucesivamente a la función encriptarPalabra.
+ * @see encriptarPalabra
+ * @param Array array que queremos encriptar
+ * @return Array array encriptado.
+ */
+ function encriptarTodasPalabras($array){
+    for($i=0; $i<count($array) ; $i++){
+        
+      $array[$i]->id=encriptarPalabra($array[$i]->id);
+    }
+    return $array;
+ }
